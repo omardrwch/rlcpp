c6174147ba7ad741e0bef4bca6e86bf494309f67
Matteo Pirotta <teopir@users.noreply.github.com>
2019-10-24 13:09:52 +0200

    Updated

--- rlcpp/include/utils/utils.h
+++ rlcpp/include/utils/utils.h
@@ -19,6 +19,7 @@ namespace utils
 template<class T>
 constexpr const T& clamp( const T& v, const T& lo, const T& hi )
 {
+    assert( !(hi < lo) );
     return (v < lo) ? lo : (hi < v) ? hi : v;
 }
 }
